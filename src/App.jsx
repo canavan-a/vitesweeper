a32540314673061c7f92edd9f18f2c0bb8121e8c
aidancanavan <aidan.canavan3@gmail.com>
2023-10-31 21:52:17 -0400

    put bombs on virtual board

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,22 @@ function App() {
   const [ySize, setYSize] = useState(10);
   const [totalBombs, setTotalBombs] = useState(20);  
   const [bombList, setBombList] = useState([]);
+  const [boardContent, setBoardContent] = useState([]);
   const [board, setBoard] = useState([]);
+
   useEffect(() => {
-    const tempBoard = Array.from({ length: ySize }, () =>
-      Array.from({ length: xSize }, () => null)
-    );
-    setBoard(tempBoard);
-    generateBombList();
+    async function initBoard(){
+      const tempBoard = Array.from({ length: ySize }, () =>
+        Array.from({ length: xSize }, () => null)
+      );
+      setBoard(tempBoard);
+      await generateBombList();
+      await generateBoardContent();
+    }
+    initBoard()
   }, [xSize, ySize]);
 
-  const generateBombList = () => {
+  async function generateBombList() {
     const allValues = Array.from({ length: xSize*ySize - 1}, (_, i) => i);
     for (let i = allValues.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -26,15 +32,37 @@ function App() {
     setBombList(allValues.slice(0, totalBombs));
   }
 
+  async function generateBoardContent(){
+    const tempBoard = Array.from({ length: ySize }, () =>
+        Array.from({ length: xSize }, () => 0)
+    );
+    let current = 0;
+    for (let i = 0; i < ySize; i++) {
+      
+      for(let j = 0; j < xSize; j++){
+        if (bombList.includes(current)){
+          tempBoard[i][j] = 'bomb';
+        }
+        else{
+
+        }
+        
+        current++;
+      }
+      
+    }
+    console.log(tempBoard);
+  }
+  
+
   return (
     <>
       <div>
 
         {board.map((row, rowIndex) => (
-          <div key={rowIndex} className="row" style={{display:'flex',
-          justifyContent: 'center'}}>
+          <div key={rowIndex} className="row" style={{display:'flex', justifyContent: 'center'}}>
             {row.map((cell, columnIndex) => (
-              <Cell key={(rowIndex*xSize)+columnIndex} x={columnIndex} y={rowIndex} id={(rowIndex*xSize)+columnIndex}
+              <Cell key={(rowIndex*xSize)+columnIndex} x={columnIndex} y={rowIndex} id={(rowIndex*xSize)+columnIndex} secret={cell}
                     bombList={bombList}></Cell>
             ))}
           </div>
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -5,23 +5,25 @@ const Cell = (props) => {
     const { y } = props;
     const { id } =  props;
     const { bombList } = props;
+    const { secret } = props;
 
     const [open, setOpen] = useState(false);
 
     const openCell = () =>{
         console.log(`${x} and ${y}`);
+        console.log(secret);
         setOpen(true);
     } 
 
     return (
         <>  
             {open?(
-            <div  style={{ width: 25, height: 25, backgroundColor: 'aaa', borderColor: 'fff', borderWidth: 1, margin: 'auto' }}>
+            <div  style={{ width: 25, height: 25, borderColor: 'fff', borderWidth: 1, margin: 'auto' }}>
                 {bombList.includes(id) ? '💣':''}
             </div>
             ):(
-                <div onClick={openCell} style={{ width: 25, height: 25, backgroundColor: 'ggg', borderColor: 'fff', borderWidth: 1, margin: 'auto' }}>
-                {id}
+                <div onClick={openCell} style={{ width: 25, height: 25, backgroundColor: 'gray', borderColor: 'fff', borderWidth: 1, margin: 'auto' }}>
+                {/* {id} */}
             </div>
             )}
         </>
