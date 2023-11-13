597c72b8cbb2356ad440ac7b8b6c01824493623b
aidancanavan <aidan.canavan3@gmail.com>
2023-11-12 20:28:17 -0500

    game context adjusted and new custom hook created

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,29 @@ import { useState, useEffect } from 'react'
 import Board from './components/MineSweeper'
 import './App.css'
 import MineSweeper from './components/MineSweeper'
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Leaderboard from './components/Leaderboard';
-
+import {GameContextProvider}  from './context/GameContext';
+import useDisableRightClick from './hooks/useDisableRightClick';
 
 function App() {
 
-  const handleContextMenu = (e) => {
-    e.preventDefault(); 
-  };
-
-  useEffect(() => {
-    window.addEventListener('contextmenu', handleContextMenu);
-
-    return () => {
-      window.removeEventListener('contextmenu', handleContextMenu);
-    };
-  }, []);
+  useDisableRightClick();
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/easy" element={<MineSweeper x={9} y={9} bombs={10} />} />
-          <Route path="/intermediate" element={<MineSweeper x={16} y={16} bombs={40} />} />
-          <Route path="/expert" element={<MineSweeper x={30} y={16} bombs={99} />} />
-          <Route path="/leaderboard" element={<Leaderboard/>} />
-        </Routes>
-      </Router>
+      <GameContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/easy" element={<MineSweeper x={9} y={9} bombs={10} />} />
+            <Route path="/intermediate" element={<MineSweeper x={16} y={16} bombs={40} />} />
+            <Route path="/expert" element={<MineSweeper x={30} y={16} bombs={99} />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+          </Routes>
+        </Router>
+      </GameContextProvider>
     </>
   )
 }
