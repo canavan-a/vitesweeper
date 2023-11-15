808b7b418f51d99d4d34bf177e0372dea42a7b75
aidancanavan <aidan.canavan3@gmail.com>
2023-11-14 22:16:55 -0500

    made backend progress, tested middleware, added env, connected to postgres

--- server/app.go
+++ server/app.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"main/controllers"
+	"main/middleware"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 
+	//load the project .env
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
 	r := gin.Default()
+
+	//testing middleware
+	r.Use(middleware.PrintMiddleware)
+
 	r.LoadHTMLGlob("templates/*")
 
 	r.Static("/static", "./static")
+
+	r.GET("/query", controllers.GetAllScores)
 
 	r.NoRoute(func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
